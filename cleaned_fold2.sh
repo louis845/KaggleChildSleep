7faea5df7558195e9d3d4f3bd24f46dd7b0d652a
louis_ml <louis321yh@gmail.com>
2023-10-07 00:39:47 +0800

    Added training scripts.

---
+++ cleaned_fold2.sh
@@ -0,0 +1,1 @@
+python training_clean_data.py --device 0 --memory_limit 0.495 --model model_fold2_clean --train_data fold_2_train --val_data fold_2_val --num_extra_steps 9 --hidden_channels 8 --hidden_blocks 4 6 8 12 8 --squeeze_excitation --use_mixup_training
