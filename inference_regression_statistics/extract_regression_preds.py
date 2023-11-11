4899dad9180f56c18d7cae5abe7868dd14e5efb4
Louis Chau <louis321yh@gmail.com>
2023-11-11 12:07:06 +0800

    Added alignment when generating prediction locations.

--- inference_regression_statistics/extract_regression_preds.py
+++ inference_regression_statistics/extract_regression_preds.py
@@ -1,8 +1,12 @@
 import os
 import shutil
+import sys
 
 import numpy as np
 import tqdm
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # add root folder to sys.path
+import postprocessing
 
 def prune(event_locs, event_vals, pruning_radius):
     descending_order = np.argsort(event_vals)[::-1]
@@ -27,6 +31,7 @@ kernel_shape = "huber"
 kernel_size = 6
 cutoff = 5.0
 pruning = 60
+alignment = True
 
 preds_folder = os.path.join("regression_labels", PREDS, "{}_kernel{}".format(kernel_shape, kernel_size))
 series_ids = [x.split(".")[0] for x in os.listdir("../individual_train_series")]
@@ -50,6 +55,16 @@ for series_id in tqdm.tqdm(series_ids):
             onset_locs = prune(onset_locs, onset_values[onset_values > cutoff], pruning)
         if len(wakeup_locs) > 0:
             wakeup_locs = prune(wakeup_locs, wakeup_values[wakeup_values > cutoff], pruning)
+
+    if alignment:
+        seconds_values = np.load("../data_naive/{}/secs.npy".format(series_id))
+        first_zero = postprocessing.compute_first_zero(seconds_values)
+        if len(onset_locs) > 0:
+            onset_kernel_values = np.load(os.path.join(preds_folder, "{}_onset.npy".format(series_id)))
+            onset_locs = postprocessing.align_predictions(onset_locs, onset_kernel_values, first_zero=first_zero)
+        if len(wakeup_locs) > 0:
+            wakeup_kernel_values = np.load(os.path.join(preds_folder, "{}_wakeup.npy".format(series_id)))
+            wakeup_locs = postprocessing.align_predictions(wakeup_locs, wakeup_kernel_values, first_zero=first_zero)
 
     np.save(os.path.join(out_folder, "{}_onset_locs.npy".format(series_id)), onset_locs)
     np.save(os.path.join(out_folder, "{}_wakeup_locs.npy".format(series_id)), wakeup_locs)
