3016ae58a0a47a942e7593aad157088334c6a839
Louis Chau <louis321yh@gmail.com>
2023-11-11 19:56:07 +0800

    Fixed a bug.

--- transform_elastic_deformation.py
+++ transform_elastic_deformation.py
@@ -32,10 +32,10 @@ def deform_v_time_series(time_series, deform_scale=True):
     if z_def_type > 0:
         if z_def_type == 1:
             z_deformation = np.random.randint(-90, 91, size=91 + 90) + np.random.rand(91 + 90)
-            ground = np.arange(start=-90, stop=91, dtype=np.float32) / 35.5195
+            ground = np.arange(start=-90, stop=91, dtype=np.float32) / 35.51
         else:
             z_deformation = (np.random.randint(-30, 31, size=31 + 30) + np.random.rand(31 + 30)) * 3
-            ground = np.arange(start=-30, stop=31, dtype=np.float32) * 3 / 35.5195
+            ground = np.arange(start=-30, stop=31, dtype=np.float32) * 3 / 35.51
 
         z_deformation = np.sort(z_deformation) / 35.52
         time_series = interp1d(ground, z_deformation, kind="cubic")(time_series)
