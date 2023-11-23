81a0f58cbdc9929b4539a1085493986ccce48201
Louis Chau <louis321yh@gmail.com>
2023-11-23 22:50:50 +0800

    Fixed a bug.

--- inference_regression_statistics/generate_balanced_folds.py
+++ inference_regression_statistics/generate_balanced_folds.py
@@ -29,10 +29,10 @@ for i in range(5):
 
     test_series_ids = folds[i]
 
-    with open("../folds/balanced5cv_fold_1_train.json", "w") as f:
+    with open("../folds/balanced5cv_fold_{}_train.json".format(i), "w") as f:
         json.dump({"dataset": train_series_ids}, f, indent=4)
 
-    with open("../folds/balanced5cv_fold_1_test.json", "w") as f:
+    with open("../folds/balanced5cv_fold_{}_test.json".format(i), "w") as f:
         json.dump({"dataset": test_series_ids}, f, indent=4)
 
     print("Fold {} train size: {}".format(i, len(train_series_ids)))
