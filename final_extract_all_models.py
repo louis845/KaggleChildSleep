f81075df546b068f73719179a8d38a9365fae180
Louis Chau <louis321yh@gmail.com>
2023-12-04 01:14:51 +0800

    Added extraction of new models.

--- final_extract_all_models.py
+++ final_extract_all_models.py
@@ -13,6 +13,13 @@ density_models_enmo = ["event5fold_density_enmo_2elastic_length3_drop_model_fold
 density_models_bdfix_swa_5cv = ["event5fold_density_bdfix_swa_2elastic_length3_drop_model_fold{}", "event5fold_density_bdfix_swa_time_2elastic_length2_drop_model_fold{}"]
 density_models_bdfix_enmo = ["event5fold_density_bdfix_enmo_2elastic_length3_drop_model_fold{}"]
 
+density_models_10cv = ["event10fold_2elastic_length3_drop_model_fold{}", "event10fold_time_2elastic_length2_drop_model_fold{}", "event10fold_enmo_2elastic_length3_drop_model_fold{}"]
+density_models_epochs = [
+    [33, 17, 14, 23, 34, 19, 14, 14, 24, 16],
+    [29, 9, 10, 20, 12, 10, 20, 5, 11, 7],
+    [12, 20, 35, 20, 14, 13, 18, 8, 6, 15]
+]
+
 out_folder = "final_models"
 
 if __name__ == "__main__":
@@ -87,3 +94,15 @@ if __name__ == "__main__":
             val_mAP = val_metrics["val_onset_dense_loc_softmax_mAP"] + val_metrics["val_wakeup_dense_loc_softmax_mAP"]
             best_model_idx = int(val_mAP.idxmax())
             shutil.copy(os.path.join(model_dir, "swa_model_{}.pt".format(best_model_idx)), os.path.join(out_folder, model_name + "_best.pt"))
+
+
+    for i, model in tqdm.tqdm(enumerate(density_models_10cv)):
+        stop_epochs = density_models_epochs[i]
+        print("Model: {}     Epochs: {}".format(model, stop_epochs))
+        for k in range(1, 11):
+            # usual model
+            model_name = model.format(k)
+            model_dir = os.path.join("models", model.format(k))
+
+            # hand picked epoch
+            shutil.copy(os.path.join(model_dir, "model_{}.pt".format(stop_epochs[k - 1])), os.path.join(out_folder, model_name + ".pt"))
